961f08790cfc20956d23379ecad461827d5bd79b
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-02-08 23:09:43 +0100

    Create FileManagerTest.java

---
+++ src/test/java/com/belicfr/pulse/FileManagerTest.java
@@ -0,0 +1,55 @@
+/*
+ * pulse - FileManagerTest
+ * 02-08-2024
+ *
+ * belicfr
+ * https://github.com/belicfr
+ * 2024 (c) All rights reserved
+ */
+
+package com.belicfr.pulse;
+
+import com.belicfr.pulse.exceptions.PulseFileNotFoundException;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileManagerTest {
+    private static final String ERROR_FILE_MUST_BE_UNDEFINED
+        = "File must be undefined.";
+
+    private static final String ERROR_FILE_MUST_EXIST
+            = "File must exist.";
+
+    /**
+     * Given: an undefined Pulse file path
+     * Then:  throws a PulseFileNotFoundException
+     * When:  we attempt to use FileManager.openFile()
+     *        with this path
+     */
+    @Test
+    void openUndefinedFile() {
+        final String WRONG_PATH = "C:\\test.pulse";
+
+        assertThrows(PulseFileNotFoundException.class, () -> {
+            FileManager.openFile(WRONG_PATH);
+        }, ERROR_FILE_MUST_BE_UNDEFINED);
+    }
+
+    /**
+     * Given: an existing Pulse file path
+     * Then:  does not throw an exception
+     * When:  we attempt to use FileManager.openFile()
+     *        with this path
+     */
+    @Test
+    void openExistingFile() {
+        final String PATH = "C:\\Users\\DÉVELOPPEMENT\\Desktop\\pulse-examples\\test.pulse";
+
+        assertDoesNotThrow(() -> {
+            FileManager.openFile(PATH);
+        }, ERROR_FILE_MUST_EXIST);
+    }
+}
