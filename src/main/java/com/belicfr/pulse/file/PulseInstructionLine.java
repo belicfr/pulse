06d99a80f9c76953a3935686008fc4124905b0cb
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-02-09 00:24:57 +0100

    Created indent management methods
    getIndentSize()
    getIndentLevel()
    isIndentValid()

--- src/main/java/com/belicfr/pulse/file/PulseInstructionLine.java
+++ src/main/java/com/belicfr/pulse/file/PulseInstructionLine.java
@@ -18,15 +18,73 @@
 
 package com.belicfr.pulse.file;
 
+import com.belicfr.pulse.exceptions.PulseInvalidIndentLevelException;
+
 import java.util.Objects;
 
+/**
+ * This class represents each line of a Pulse source code
+ * file.
+ * <p>
+ * It allows to perform operations specific to these lines.
+ *
+ * @author belicfr
+ */
 public class PulseInstructionLine {
+    public static final int INDENT_UNIT_SIZE = 4;
+
+    public static final char INDENT_UNIT_CHARACTER = ' ';
+
     private String line;
 
     public PulseInstructionLine(String line) {
         this.line = line;
+    }
 
-        // TODO: create pulse line attributes.
+    /**
+     * @return Line indent size
+     */
+    public int getIndentSize() {
+        int indentUnitCount;
+        indentUnitCount = 0;
+
+        for (char character: this.getLine().toCharArray()) {
+            if (character == INDENT_UNIT_CHARACTER) {
+                indentUnitCount++;
+            } else {
+                break;
+            }
+        }
+
+        return indentUnitCount;
+    }
+
+    /**
+     * Measures and returns the indent level.
+     *
+     * @return Indent level
+     * @throws PulseInvalidIndentLevelException If indent
+     *                                          level is
+     *                                          invalid
+     */
+    public int getIndentLevel()
+    throws PulseInvalidIndentLevelException {
+        int indentSize;
+        indentSize = this.getIndentSize();
+
+        if (!this.isIndentValid()) {
+            throw new PulseInvalidIndentLevelException(this.getLine());
+        }
+
+        return indentSize / INDENT_UNIT_SIZE;
+    }
+
+    /**
+     * @return If indent is valid (packs of
+     *         INDENT_UNIT_SIZE spaces).
+     */
+    public boolean isIndentValid() {
+        return this.getIndentSize() % INDENT_UNIT_SIZE == 0;
     }
 
     /**
