706c714fd9253ee07c0cc12f7a9d01ec6023976b
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-02-08 23:09:18 +0100

    Create PulseFile.java

---
+++ src/main/java/com/belicfr/pulse/file/PulseFile.java
@@ -0,0 +1,101 @@
+/*
+ * pulse - PulseFile
+ * 02-08-2024
+ *
+ * belicfr
+ * https://github.com/belicfr
+ * 2024 (c) All rights reserved
+ */
+
+/*
+ * pulse - PulseFile
+ * 02-08-2024
+ *
+ * belicfr
+ * https://github.com/belicfr
+ * 2024 (c) All rights reserved
+ */
+
+package com.belicfr.pulse.file;
+
+import com.belicfr.pulse.exceptions.PulseUnreadableFileException;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PulseFile {
+    /** File path */
+    private String path;
+
+    public PulseFile(String path) {
+        this.path = path;
+    }
+
+    /**
+     * Attempt to read Pulse file and return its lines.
+     *
+     * @return Pulse file lines
+     * @throws PulseUnreadableFileException If given file cannot be
+     *                             read
+     */
+    private List<String> getFileLines()
+    throws PulseUnreadableFileException {
+        Path path;
+
+        path = Paths.get(this.getPath());
+
+        try {
+            return Files.readAllLines(path, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        throw new PulseUnreadableFileException(this.getPath());
+    }
+
+    /**
+     * @return File path
+     */
+    public String getPath() {
+        return this.path;
+    }
+
+    /**
+     * @return File content
+     * @throws PulseUnreadableFileException If file cannot be read
+     */
+    public String getContent()
+    throws PulseUnreadableFileException {
+        StringBuilder content;
+        content = new StringBuilder();
+
+        for (String line: this.getFileLines()) {
+            content.append(line).append('\n');
+        }
+
+        return content.toString();
+    }
+
+    /**
+     * Reads file and returns a Pulse lines objects list.
+     *
+     * @return Pulse lines objects list
+     * @throws PulseUnreadableFileException If file cannot be read
+     */
+    public List<PulseInstructionLine> getCode()
+    throws PulseUnreadableFileException {
+        List<PulseInstructionLine> code;
+        code = new ArrayList<>();
+
+        for (String line: this.getFileLines()) {
+            code.add(new PulseInstructionLine(line));
+        }
+
+        return code;
+    }
+}
