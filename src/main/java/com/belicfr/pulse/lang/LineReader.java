2ffc708155f9e452122b3fc66afe4996db37b7f2
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-02-09 11:45:34 +0100

    Create LineReader.java

---
+++ src/main/java/com/belicfr/pulse/lang/LineReader.java
@@ -0,0 +1,15 @@
+/*
+ * pulse - LineReader
+ * 02/09/2024
+ *
+ * belicfr
+ * https://github.com/belicfr
+ * 2024 (c) All rights reserved
+ */
+
+package com.belicfr.pulse.lang;
+
+public class LineReader {
+    public static final String CASE_VARIABLE_DEFINITION
+        = "VARIABLE_DEFINITION";
+}
