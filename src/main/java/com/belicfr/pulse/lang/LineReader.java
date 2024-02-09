f1c32658f4b687445051e0b45ffccdcde678200b
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-02-09 17:30:56 +0100

    WIP LineReader

--- src/main/java/com/belicfr/pulse/lang/LineReader.java
+++ src/main/java/com/belicfr/pulse/lang/LineReader.java
@@ -9,7 +9,74 @@
 
 package com.belicfr.pulse.lang;
 
+import com.belicfr.pulse.file.PulseInstructionLine;
+import com.belicfr.pulse.heap.Heap;
+import com.belicfr.pulse.lang.types.Integer;
+import com.belicfr.pulse.lang.types.Type;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class LineReader {
-    public static final String CASE_VARIABLE_DEFINITION
-        = "VARIABLE_DEFINITION";
+    private static final String REGEX_VARIABLE_DEFINITION
+        = "([a-z])([a-z0-9]*)(\s*=\s*)(.+)";
+
+    private Heap fileHeap;
+
+    private PulseInstructionLine line;
+
+    public LineReader(Heap fileHeap, PulseInstructionLine line) {
+        this.fileHeap = fileHeap;
+        this.line     = line;
+    }
+
+    public void read() {
+        // Variable definition?
+
+        // TODO...
+    }
+
+    private void variableDefinition() {
+        List<String> lineParts;
+
+
+
+        Pattern variableDefinitionPattern;
+        Matcher variableDefinitionMatcher;
+
+        lineParts = this.getLine().getSplitParts();
+
+        variableDefinitionPattern = Pattern.compile(REGEX_VARIABLE_DEFINITION);
+        variableDefinitionMatcher
+            = variableDefinitionPattern.matcher(this.getLine().toString());
+
+        if (variableDefinitionMatcher.matches()) {
+            this.getFileHeap().add(lineParts.get(0),
+                                   );
+        }
+    }
+
+    private Class guessType() {
+        String lineContent;
+        lineContent = this.line.getContent();
+
+        if (Integer.isCompatible(lineContent)) {
+            return Integer.class;
+        }
+    }
+
+    /**
+     * @return File heap object
+     */
+    public Heap getFileHeap() {
+        return this.fileHeap;
+    }
+
+    /**
+     * @return Current read line
+     */
+    public PulseInstructionLine getLine() {
+        return this.line;
+    }
 }
