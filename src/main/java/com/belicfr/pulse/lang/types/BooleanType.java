092f387a82dc816c02a225653c7960a3079130e6
Jonathan <96206878+belicfr@users.noreply.github.com>
2024-02-10 03:09:31 +0100

    Create BooleanType.java

---
+++ src/main/java/com/belicfr/pulse/lang/types/BooleanType.java
@@ -0,0 +1,68 @@
+/*
+ * pulse - Integer
+ * 02/09/2024
+ *
+ * belicfr
+ * https://github.com/belicfr
+ * 2024 (c) All rights reserved
+ */
+
+package com.belicfr.pulse.lang.types;
+
+import com.belicfr.pulse.exceptions.PulseCannotStoreAsGivenTypeException;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BooleanType extends Type implements TypeInterface {
+    private static final String REGEX_NUMERIC
+        = "^(true|false)$";
+
+    private boolean value;
+
+    public BooleanType(String expression)
+    throws PulseCannotStoreAsGivenTypeException {
+        super(expression);
+
+        this.store();
+    }
+
+    /**
+     * Attempts to store given expression as an integer
+     * into Pulse heap.
+     *
+     * @throws PulseCannotStoreAsGivenTypeException If expression is not
+     *                                              numeric
+     */
+    private void store()
+    throws PulseCannotStoreAsGivenTypeException {
+        if (!isCompatible(this.getExpression())) {
+            throw new PulseCannotStoreAsGivenTypeException(
+                this.getExpression(), getClass());
+        }
+
+        this.value = Boolean.parseBoolean(super.getExpression());
+    }
+
+    /**
+     * @param expression Given expression
+     * @return If given expression is numeric
+     */
+    public static boolean isCompatible(String expression) {
+        Pattern numericPattern;
+        Matcher numericMatcher;
+
+        numericPattern = Pattern.compile(REGEX_NUMERIC);
+        numericMatcher = numericPattern.matcher(expression);
+
+        return numericMatcher.matches();
+    }
+
+    /**
+     * @return Current value
+     */
+    @Override
+    public Boolean getValue() {
+        return this.value;
+    }
+}
